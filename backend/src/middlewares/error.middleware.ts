e8bb92c675cbd976be23acd22ce7fb4f36836ae4
manhtranduc <manhtranduc0202@gmail.com>
2025-04-10 18:10:21 +0700

    handle error

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,22 @@
+import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next): any => {
+  if (err instanceof SyntaxError) {
+    return res.status(HTTPSTATUS.BAD_REQUEST).json({
+      message: "Invalid JSON format. Please check your request body.",
+    });
+  }
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+      errorCode: err.errorCode,
+    });
+  }
+  return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+    message: "Internal Server Error",
+    error: err?.message || "Unknow error occurred",
+  });
+};
