5638441c8cfe2bdbffef016b4b1b42a9d658b98c
manhtranduc <manhtranduc0202@gmail.com>
2025-04-11 20:48:29 +0700

    login gg by partportjs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,12 @@ import session from "cookie-session";
 import { config } from "./config/app.config";
 import connectDatabase from "./config/database.config";
 import { errorHandler } from "./middlewares/error.middleware";
+import passport from "passport";
+import authRoutes from "./routes/auth.route";
+import "./config/partport.config";
 const app = express();
 
-const BASE_APP = config.BASE_PATH;
+const BASE_PATH = config.BASE_PATH;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,10 +24,13 @@ app.use(
     sameSite: "lax",
   })
 );
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors({ origin: config.FRONTEND_ORIGIN, credentials: true }));
 
+app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(errorHandler);
+
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
   await connectDatabase();
---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+import UserModel from "../models/user.model";
+import AccountModel from "../models/account.model";
+import WorkspaceModel from "../models/workspace.model";
+import RoleModel from "../models/role-permission.model";
+import { Roles } from "../enums/role.enum";
+import { NotFoundException } from "../utils/appError";
+import MemberModel from "../models/member.model";
+
+export const loginOrCreateAccountService = async (data: {
+  provider: string;
+  displayName: string;
+  providerId: string;
+  picture?: string;
+  email?: string;
+}) => {
+  const { provider, displayName, providerId, picture, email } = data;
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    let user = await UserModel.findOne({ email }).session(session);
+    if (!user) {
+      user = new UserModel({
+        email,
+        name: displayName,
+        profilePicture: picture || null,
+      });
+      await user.save({ session });
+
+      const account = new AccountModel({
+        userId: user._id,
+        provider: provider,
+        providerId: providerId,
+      });
+      await account.save({ session });
+
+      // 3. Create a new workspace for the new user
+      const workspace = new WorkspaceModel({
+        name: `My Workspace`,
+        description: `Workspace created for ${user.name}`,
+        owner: user._id,
+      });
+      await workspace.save({ session });
+      // lấy ra cái role owner
+      const ownerRole = await RoleModel.findOne({
+        name: Roles.OWNER,
+      }).session(session);
+
+      if (!ownerRole) {
+        throw new NotFoundException("Owner role not found");
+      }
+      // tạo member với role owner của workspace
+      const member = new MemberModel({
+        userId: user._id,
+        workspaceId: workspace._id,
+        role: ownerRole._id,
+        joinedAt: new Date(),
+      });
+      await member.save({ session });
+
+      // cập nhật lại currentWorkspace của user
+      user.currentWorkspace = workspace._id as mongoose.Types.ObjectId;
+      await user.save({ session });
+    }
+    await session.commitTransaction();
+    session.endSession();
+    return { user };
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+};
